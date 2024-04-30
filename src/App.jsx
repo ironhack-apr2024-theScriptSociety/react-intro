1c8c80146ffdd3e3137df83076dbf8604d051bd8
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-04-30 10:26:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>React Intro</h1>
+    </>
+  )
+}
+
+export default App;
